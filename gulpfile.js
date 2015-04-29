c3bd68da51e9f924b16e6d66b2eff4cf1ecc9d90
Jefferson Lam <jeffersonwlam@gmail.com>
2015-04-29 00:27:49 -0500

    Add mock nav v2

--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,7 @@ gulp.task('html', function() {
 gulp.task('jshint', function() {
   gulp.src('js/*.js')
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('watch', function() {
