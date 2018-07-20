714146b169d913be647623c97a7a09cae5446130
Jefferson Lam <jeffersonwlam@gmail.com>
2018-07-20 17:19:42 -0500

    Prettier

--- js/app.js
+++ js/app.js
@@ -1,35 +1,41 @@
 'use strict';
 
 //This code executes on load
-(function(){
+(function() {
   console.log('Hello, developer!');
-  createEmailLink(['jefferson','w','lam','@','gmail','.','com'], 'email-link');
-
-  function createEmailLink(array, anchorId){
+  createEmailLink(
+    ['jefferson', 'w', 'lam', '@', 'gmail', '.', 'com'],
+    'email-link'
+  );
+  function createEmailLink(array, anchorId) {
     var email = array.join('');
-    document.getElementById(anchorId).href = 'mailto:' + email + '?subject=Reaching Out';
+    document.getElementById(anchorId).href =
+      'mailto:' + email + '?subject=Reaching Out';
   }
-}());
+})();
 
 //This code is used live by the DOM
-function toggleDarkUI(){
+function toggleDarkUI() {
   var elem = document.body;
   toggleClass(elem, 'is-dark-ui');
 }
 
-function toggleClass(element, className){
-  if (!element || !className){
+function toggleClass(element, className) {
+  if (!element || !className) {
     return;
   }
 
-  var classString = element.className, nameIndex = classString.indexOf(className);
+  var classString = element.className,
+    nameIndex = classString.indexOf(className);
   //if class doesn't exist, add it
   if (nameIndex == -1) {
     classString += ' ' + className;
   }
   //if class does exist, remove it
   else {
-    classString = classString.substr(0, nameIndex-1) + classString.substr(nameIndex+className.length);
+    classString =
+      classString.substr(0, nameIndex - 1) +
+      classString.substr(nameIndex + className.length);
   }
   element.className = classString;
 }
